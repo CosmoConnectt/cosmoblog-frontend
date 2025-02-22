7509b92149c3102e142065545d11fd99c99b6a19
CosmoConnectt <projectuse882@gmail.com>
2025-02-22 17:29:12 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Homepage from "./routes/Homepage";
+import SinglePostPage from "./routes/SinglePostPage";
+import PostListPage from "./routes/PostListPage";
+import Write from "./routes/Write"; // Import the Write component
 
 const App = () => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
-      {/* NAVBAR */}
-      <Navbar />
+    <Router>
+      <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+        {/* NAVBAR */}
+        <Navbar />
 
-      {/* BREADCRUMB */}
-      {/* INTRODUCTION */}
-      {/* FEATURED POSTS */}
-      {/* POST LIST */}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/posts/:slug" element={<SinglePostPage />} /> {/* Define the SinglePostPage route */}
+          <Route path="/posts" element={<PostListPage />} />
+          <Route path="/write" element={<Write />} /> {/* Define the Write route */}
+        </Routes>
+
+        {/* BREADCRUMB */}
+        {/* INTRODUCTION */}
+        {/* FEATURED POSTS */}
+        {/* POST LIST */}
       </div>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
